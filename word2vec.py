5fa7f8796591dca6119bd64a8d6cad81831549a9
youssefhakam <88097330+youssefhakam@users.noreply.github.com>
2024-06-01 16:24:28 +0100

    Update word2vec.py

--- word2vec.py
+++ word2vec.py
@@ -6,8 +6,8 @@ from datasets import load_dataset
 
 ### Define Hyperparametre 
 embedding_size = 100  ## Dimensionality of word embedding
-vocab_size = 10000 ## Number of words in vocabulary
-window_size = 2 ## Context window size
+window_size = 3 ## Context window size
+num_neg_samples = 2 # Number of negative samples
 
 
 datasets = load_dataset("atlasia/darija-translation")
@@ -34,3 +34,62 @@ vocab_size = len(vocab)
 
 word2idx = {word: idx for idx, word in enumerate(vocab)}
 idx2word = {idx: word for word, idx in word2idx.items()}
+
+def create_context_target_pairs(phrases, window_size):
+    context_target_pairs = []
+    for phrase in phrases:
+        words = phrase.split()
+        for i in range(window_size, len(words) - window_size):
+            context = words[i-window_size:i] + words[i+1:i+1+window_size]
+            target = words[i]
+            context_target_pairs.append((context, target))
+    return context_target_pairs
+
+def get_negative_samples(target_idx, vocab_size, num_neg_samples):
+    neg_samples = []
+    while len(neg_samples) < num_neg_samples:
+        neg_sample = np.random.randint(0, vocab_size)
+        if neg_sample != target_idx:
+            neg_samples.append(neg_sample)
+    return neg_samples
+
+class CBOWModel(nn.Module):
+    def __init__(self, vocab_size, embedding_dim):
+        super(CBOWModel, self).__init__()
+        self.embeddings = nn.Embedding(vocab_size, embedding_dim)
+        self.linear = nn.Linear(embedding_dim, vocab_size)
+    
+    def forward(self, context_idxs):
+        embeds = self.embeddings(context_idxs)
+        context_vec = torch.mean(embeds, dim=0).view((1, -1))
+        out = self.linear(context_vec)
+        log_probs = torch.log_softmax(out, dim=1)
+        return log_probs
+
+model = CBOWModel(vocab_size, embedding_size)
+loss_function = nn.NLLLoss()
+optimizer = optim.SGD(model.parameters(), lr=0.001)
+# This determines the number of context words on each side of the target word
+context_target_pairs = create_context_target_pairs(df, window_size)
+
+for epoch in range(2):
+    total_loss = 0
+    for context, target in context_target_pairs:
+        context_idxs = torch.tensor([word2idx[w] for w in context], dtype=torch.long)
+        target_idx = torch.tensor([word2idx[target]], dtype=torch.long)
+        
+        model.zero_grad()
+        
+        log_probs = model(context_idxs)
+        loss = loss_function(log_probs, target_idx)
+        
+        loss.backward()
+        optimizer.step()
+        
+        total_loss += loss.item()
+    if (epoch + 1) % 10 == 0:
+        print(f'Epoch {epoch+1}, Loss: {total_loss}')
+embedding_matrix = model.embeddings.weight.data
+embedding_matrix_np = embedding_matrix.numpy()
+for word, idx in word2idx.items():
+    print(f"Embedding for {word}: {embedding_matrix_np[idx]}")
